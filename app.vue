4f99c78c34da2515d2f8397472fb91effa6f8cf7
BQBZKY <91907825+BOBZKY@users.noreply.github.com>
2023-03-22 16:46:36 +0300

    RESET

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="App">
+  <div id="app">
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,3 @@
-import postcssConfig from './postcss.config'
-
 export default defineNuxtConfig({
-  css: [
-    '~/styles/tailwind.scss',
-  ],
 
-  postcss: postcssConfig,
-
-  components: {
-    dirs: [
-      {path: '~/components/formFields', pathPrefix: false},
-    ],
-  },
-
-  experimental: {
-    reactivityTransform: true,
-  },
-
-  modules: [
-    'nuxt-icon',
-    '@vueuse/nuxt',
-  ],
 })
